5d904dfc0e7c9ee47fac1ec5cdfee0b8deab66c6
ManasKapoor18 <manasmk2016@gmail.com>
2023-11-06 12:33:57 +0530

    added passportjs and oauth functionalities

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientId,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("accessToken: ", accessToken);
+      console.log("refreshToken: ", refreshToken);
+      console.log("profile: ", profile);
+      console.log("done: ", done);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 app.listen(5000, () => {
   console.log(`Server listening on 5000`);
